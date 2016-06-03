9f705db5e361936176c1d578217cace80cd99f40
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-06-03 15:00:09 -0300

    Fix clean_run.sh

--- scripts/clean_run.sh
+++ scripts/clean_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for ip in `cut -f1 -d" " hostfile.txt`; do
-  ssh "${ip}" "killall sar; rm -rf *.log *.sa cpuinfo_* lscpu_*"
+  ssh "${ip}" "killall sar; cd `pwd`; rm -rf *.log *.sa cpuinfo_* lscpu_*"
 done
